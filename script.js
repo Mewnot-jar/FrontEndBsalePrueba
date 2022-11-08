4939c42dd0760eaa02fed9367b81ca722f97564f
Martin Ardiles <martinardiles.a@gmail.com>
2022-11-08 19:38:43 -0300

    Busca el producto segun el input y valida si es que este esta vacio

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
 const content = document.querySelector('#content')
 const categories = document.querySelectorAll('.category')
+const homeBtn = document.querySelector('#home_button')
 var contenedor = document.getElementById('contenedor_carga')
+const inputSearch = document.getElementById('input_search')
+const btnSearch = document.getElementById('button_search')
+btnSearch.disabled = true
 document.addEventListener('DOMContentLoaded', function(){
     getProducts()
+    disableBtnSearch()
 })
 function getProducts(category = null){
     content.innerHTML = ''
@@ -19,16 +24,18 @@ function fill(data){
     content.innerHTML = ''
     data.forEach(d => {
         content.innerHTML += `
-            <div class="card col" style="width: 18rem;">
-                <img src="${d.url_image}" class="card-img-top " alt="product_image">
-                <div class="card-body">
-                    <h5 class="card-title">${d.name}</h5>
-                    <div class="d-flex justify-content-around">
-                        <p class="card-text">${d.discount ? '<del>' + d.price + '</del>' : d.price}</p>
-                        ${d.discount ? '<p class="card-text">'+ (d.price * d.discount)/100 +'</p>': ''}
-                        ${d.discount ? '<p class="card-text">' + d.discount + '%</p>' : ''}
+            <div class="card-deck">
+                <div class="card">
+                    <img src="${d.url_image}" class="card-img-top " alt="product_image">
+                    <div class="card-body">
+                        <h5 class="card-title">${d.name}</h5>
+                        <div class="d-flex justify-content-around mb-3">
+                            <p class="card-text">${d.discount ? '<del>' + '$' + d.price + '</del>' : '$' + d.price}</p>
+                            ${d.discount ? '<p class="card-text">'+ '$' + Math.round(( d.price - (d.price * d.discount)/100 )) +'</p>': ''}
+                            ${d.discount ? '<p class="card-text">' + d.discount + '%</p>' : ''}
+                        </div>
+                        <center><a href="#" class="btn btn-primary">Comprar</a></center>
                     </div>
-                    <a href="#" class="btn btn-primary">Comprar</a>
                 </div>
             </div>
         `
@@ -39,6 +46,9 @@ for (let i = 0; i < categories.length; i++) {
         getProducts(i+1)
     })
 }
+homeBtn.addEventListener('click', function(){
+    getProducts()
+})
 function loadingAnimation(){
     contenedor.style.visibility = 'visible';
     contenedor.style.opacity = '100'
@@ -47,3 +57,23 @@ function loadingAnimation(){
         contenedor.style.opacity = '0'
     }, 2500);
 }
+function searchProducts(search){
+    content.innerHTML = ''
+    fetch(`http://localhost:8000/api/products/search/${search ? search : ''}`)
+    .then(res => res.json())
+    .then(data => {
+        fill(data)
+    })
+    loadingAnimation()
+}
+btnSearch.addEventListener('click', function(e){
+    e.preventDefault()
+    searchProducts(inputSearch.value)
+})
+inputSearch.addEventListener('keyup', function(){
+    if(!inputSearch.value){
+        btnSearch.disabled = true
+    }else{
+        btnSearch.disabled = false
+    }
+})
