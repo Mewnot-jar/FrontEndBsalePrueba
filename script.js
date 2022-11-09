fb5024c9501cb1437c7dc4bb72f7abdeba23a8b5
Martin Ardiles <martinardiles.a@gmail.com>
2022-11-09 11:51:21 -0300

    Cambios en estilos de botones y margen de cada card

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ function fill(data){
     content.innerHTML = ''
     data.forEach(d => {
         content.innerHTML += `
-            <div class="card-group">
-                <div class="card" style="height: 30rem">
+            <div>
+                <div class="card m-2" style="height: 30rem">
                     <div class="card-body">
                         <div style="height: 60%;">
                             <img src="${d.url_image}" class="card-img-top " alt="product_image" style="height: 100%;">
@@ -45,7 +45,18 @@ function fill(data){
                                 </div>
                             </div>
                         </div>
-                        <center><a href="#" class="btn btn-primary">Comprar</a></center>
+                        <center>
+                            <a href="#" class="btn btn-success">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <circle cx="6" cy="19" r="2" />
+                                <circle cx="17" cy="19" r="2" />
+                                <path d="M17 17h-11v-14h-2" />
+                                <path d="M6 5l6.005 .429m7.138 6.573l-.143 .998h-13" />
+                                <path d="M15 6h6m-3 -3v6" />
+                                </svg>
+                            </a>
+                        </center>
                     </div>
                 </div>
             </div>
