cb5ce77a215eb31beb83a742e468bc983b2f8c66
Martin Ardiles <martinardiles.a@gmail.com>
2022-11-09 21:00:07 -0300

    Cambio en estilos y en la forma en que se traen las categorias

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const content = document.querySelector('#content')
-const categories = document.querySelectorAll('.category')
+const categoryContainer = document.getElementById('categories')
 const homeBtn = document.querySelector('#home_button')
 var contenedor = document.getElementById('contenedor_carga')
 const inputSearch = document.getElementById('input_search')
@@ -9,8 +9,10 @@ let mensajeError
 btnSearch.disabled = true
 document.addEventListener('DOMContentLoaded', function(){
     getProducts()
+    getCategories()
 })
 function getProducts(category = null){
+    inputEmpty()
     contenedor.style.visibility = 'visible';
     contenedor.style.opacity = '100'
     content.innerHTML = ''
@@ -25,7 +27,19 @@ function getProducts(category = null){
         }
     })
 }
-
+function getCategories(){
+    fetch(`https://backendbsaleprueba-production.up.railway.app/api/fillCategories`)
+    .then(res => res.json())
+    .then(data => {
+        fillCategories(data)
+        let categories = document.querySelectorAll('.category')
+        for (let i = 0; i < categories.length; i++) {
+            categories[i].addEventListener('click', function(){
+                getProducts(i+1)
+            })
+        }
+    })
+}
 function fill(data){ 
     content.innerHTML = ''
     data.forEach(d => {
@@ -34,7 +48,7 @@ function fill(data){
                 <div class="card m-2" style="height: 30rem">
                     <div class="card-body">
                         <div style="height: 60%;">
-                            <img src="${d.url_image}" class="card-img-top " alt="product_image" style="height: 100%;">
+                            <img src="${d.url_image}" class="card-img-top " alt="product_image">
                         </div>
                         <div style="height: 9rem">
                             <h5 class="card-title">${d.name}</h5>
@@ -47,7 +61,7 @@ function fill(data){
                             </div>
                         </div>
                         <center>
-                            <a href="#" class="btn btn-success">
+                            <a href="#" class="btn btn-primary">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                                 <circle cx="6" cy="19" r="2" />
@@ -64,10 +78,12 @@ function fill(data){
         `
     });
 }
-for (let i = 0; i < categories.length; i++) {
-    categories[i].addEventListener('click', function(){
-        getProducts(i+1)
-    })
+function fillCategories(data){ 
+    data.forEach(d => {
+        categoryContainer.innerHTML += `
+            <a class="dropdown-item category" href="#">${d.name.toUpperCase()}</a>
+        `
+    });
 }
 homeBtn.addEventListener('click', function(){
     getProducts()
@@ -82,13 +98,11 @@ function searchProducts(search){
         if(data.length){
             fill(data)
             document.querySelector('#alert-container').style.display = 'none'
-            console.log('hay cosas')
             contenedor.style.visibility = 'hidden';
             contenedor.style.opacity = '0'
         }else{
             document.querySelector('#alert-container').style.display = 'flex'
             document.querySelector('#alert-container').textContent = 'No se encontro el producto 🥸'
-            console.log('no hay cosas')
             contenedor.style.visibility = 'hidden';
             contenedor.style.opacity = '0'
         }
@@ -97,15 +111,16 @@ function searchProducts(search){
 btnSearch.addEventListener('click', function(e){
     e.preventDefault()
     searchProducts(inputSearch.value)
-    inputSearch.value = ''
 })
-inputSearch.addEventListener('keyup', function(){
+inputSearch.addEventListener('keyup', inputEmpty)
+function inputEmpty(){
     if(!inputSearch.value){
         btnSearch.disabled = true
     }else{
         btnSearch.disabled = false
     }
-})
+}
+
 function numericFormat(number){
     let numberParts = number.toString().split('.')
     numberParts[0] = numberParts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
