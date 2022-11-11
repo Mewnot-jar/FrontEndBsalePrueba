ef8a79fb2f2ccfb9f218440a344bec02252cb8d8
Martin Ardiles <martinardiles.a@gmail.com>
2022-11-10 21:50:13 -0300

    cambio links

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function getProducts(category = null) {
     contenedor.style.visibility = 'visible';
     contenedor.style.opacity = '100'
     content.innerHTML = ''
-    fetch(`http://localhost:8000/api/products/${category ? category : ''}`)
+    fetch(`https://backendbsaleprueba-production.up.railway.app/api/products/${category ? category : ''}`)
         .then(res => res.json())
         .then(data => {
             productos = [
@@ -47,7 +47,7 @@ function getProducts(category = null) {
         })
 }
 function getCategories() {
-    fetch(`http://localhost:8000/api/fillCategories`)
+    fetch(`https://backendbsaleprueba-production.up.railway.app/api/fillCategories`)
         .then(res => res.json())
         .then(data => {
             fillCategories(data)
@@ -140,7 +140,7 @@ function searchProducts(search) {
     contenedor.style.visibility = 'visible';
     contenedor.style.opacity = '100'
     content.innerHTML = ''
-    fetch(`http://localhost:8000/api/products/search/${search ? search : ''}`)
+    fetch(`https://backendbsaleprueba-production.up.railway.app/api/products/search/${search ? search : ''}`)
         .then(res => res.json())
         .then(data => {
             if (data.length) {
